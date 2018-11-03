04eab411e4607ac1f7155e406d44838196bf9c74
d-lean <dlean@jbnote.com>
2018-11-03 21:40:25 +0300

    lab2_1 (Huffman coding)

---
+++ lab2_1/HuffmanNode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2_1
+{
+    class HuffmanNode
+    {
+        public int frequency;
+        public string value;
+        public HuffmanNode leftLeaf;
+        public HuffmanNode rightLeaf;
+        
+
+        public HuffmanNode(int frequency, string value, HuffmanNode leftLeaf, HuffmanNode rightLeaf)
+        {
+            this.frequency = frequency;
+            this.value = value;
+            this.leftLeaf = leftLeaf;
+            this.rightLeaf = rightLeaf;
+        }
+
+        public string[] getEncoding(string bits)
+        {
+            if (leftLeaf == null && rightLeaf == null)
+            {
+                return new string[] { this.value + ": " + bits };
+            }
+            else
+            {
+                return leftLeaf.getEncoding(bits + "0")
+                    .Concat(rightLeaf.getEncoding(bits + "1"))
+                    .ToArray();
+            }
+        }
+    }
+}
